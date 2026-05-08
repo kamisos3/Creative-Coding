f5adb78701e48d1aef7defb86a21b7fe195c149d
kamisos3 <kamilasostre11@gmail.com>
2026-05-07 23:06:44 -0400

    Completed movement and mold drawing, but it takes a while to form

--- p5.js/mold-pattern/mold.js
+++ p5.js/mold-pattern/mold.js
@@ -15,6 +15,7 @@ class Mold {
         // Sensor vars
         this.rSensorPos = createVector(0, 0);
         this.lSensorPos = createVector(0, 0);
+        this.fSensorPos = createVector(0, 0);
         this.fSensorAngle = 45;
         this.sensorDist = 10;
     }
@@ -33,14 +34,50 @@ class Mold {
         this.x = (this.x + this.vx + width) % width;
         this.y = (this.y + this.vy + height) % height;
 
-        
-    }
-     
-    display() {
-        fill(0);
-        ellipse(this.x, this.y, this.r*2, this.r*2);
+        // Creates 3 sensor positions
+        this.getSensorPos(this.rSensorPos, this.heading + this.sensorAngle);
+        this.getSensorPos(this.lSensorPos, this.heading - this.sensorAngle);
+        this.getSensorPos(this.fSensorPos, this.heading);
 
-        
+        // Get indices for the sensors pos, anf color value for the indices
+        let index, l, r, f;
+        index = 4 * (d * floor(this.rSensorPos.y)) * (d * width) + 4 * (d * floor(this.rSensorPos.x));
+        r = pixels[index];
+
+        index = 4 * (d * floor(this.lSensorPos.y)) * (d * width) + 4 * (d * floor(this.lSensorPos.x));
+        l = pixels[index];
+
+        index = 4 * (d * floor(this.fSensorPos.y)) * (d * width) + 4 * (d * floor(this.fSensorPos.x));
+        f = pixels[index];
+
+        // Compares values r, l, f to move the sensors
+        if (f > l && f > r) {
+            this.heading += 0;
+        } else if (f < l && f < r) {
+            if (random(1) < 0.5) {
+                this.heading += this.rotAngle;
+            } else {
+                this.heading -= this.rotAngle;
+            }
+        } else if (l > r) {
+            this.heading += -this.rotAngle;
+        } else if (r > l) {
+            this.heading += this.rotAngle;
+        }
+
     }
-    
+
+    display() {
+        noStroke();
+        fill(255);
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+
+
+    }
+
+    getSensorPos(sensor, angle) {
+        sensor.x = (this.x + this.sensorDist * cos(angle) + width) % width;
+        sensor.y = (this.y + this.sensorDist * sin(angle) + height) % height;
+    }
+
 }
--- p5.js/mold-pattern/sketch.js
+++ p5.js/mold-pattern/sketch.js
@@ -1,13 +1,30 @@
-let m;
+let molds = []; let num = 4000;
+let d;
 
 function setup() {
     createCanvas(400, 400);
     angleMode(DEGREES);
+    d = pixelDensity();
 
-    m = new Mold();
+    for (let i = 0; i < num; i++) {
+        molds[i] = new Mold();
+    }
 }
 
 function draw() {
-    background(220);
-    m.display();
+    background(0, 5);
+    loadPixels();
+
+    for (let i = 0; i < num; i++) {
+        if (key == "s") {
+            molds[i].stop = true;
+            updatePixels();
+            noLoop();
+        } else {
+            molds[i].stop = false;
+        }
+
+        molds[i].update();
+        molds[i].display();
+    }
 }
